6f8b894647d6f72e02b8d1d731592e2c276265b0
liusnew <liushuinew@163.com>
2024-12-31 16:40:36 +0800

    fix: 环境更新的接口参数修改

--- frontend/src/commands/environment.ts
+++ frontend/src/commands/environment.ts
@@ -117,8 +117,9 @@ export const environment_batch_create = async (environmentNames: Array<String>):
     return await invoke('environment_batch_create', { environmentNames })
 }
 
-export const environment_modify_info = async (payload: EnvironmentInfo): Promise<any> => {
-    return await invoke('environment_modify_info ', { payload })
+export const environment_modify_info = async (
+    envirunmentUuid: String, payload: EnvironmentInfo): Promise<any> => {
+    return await invoke('environment_modify_info ', { envirunmentUuid, payload })
 };
 
 export const environment_modify_basic_info = async (environmentUuid: string, payload: Environment): Promise<any> => {
