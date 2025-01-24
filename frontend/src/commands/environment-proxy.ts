c53345bc67ddfbb111e5cacba0594c5f9522a482
liusnew <liushuinew@163.com>
2025-01-24 18:37:54 +0800

    feat: 自动更新 1.0

--- frontend/src/commands/environment-proxy.ts
+++ frontend/src/commands/environment-proxy.ts
@@ -29,8 +29,8 @@ export const environment_proxies_create = async (payload: Proxy): Promise<any> =
     })
 };
 
-export const environment_proxies_modify = async (id: number, payload: Proxy): Promise<any> => {
-    return await invoke('environment_proxies_modify', { id, payload })
+export const environment_proxies_modify = async (payload: Proxy): Promise<any> => {
+    return await invoke('environment_proxies_modify', { payload })
 };
 
 export const environment_modify_proxy = async (environmentUuid: string, payload: Proxy): Promise<any> => {
--- frontend/src/components/updator.vue
+++ frontend/src/components/updator.vue
@@ -1,7 +1,48 @@
 <script setup lang="ts">
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+import { onMounted } from "vue";
 
+onMounted(async () => {
+  const update = await check();
+  if (!update) return;
+
+  console.log(
+    `Found update ${update.version} from ${update.date} with notes ${update.body}`
+  );
+
+  let downloaded = 0;
+  let contentLength = 0;
+
+  // 下载更新
+  await update.download((event) => {
+    switch (event.event) {
+      case "Started":
+        contentLength = event.data.contentLength as number;
+        console.log(`Started downloading ${event.data.contentLength} bytes`);
+        break;
+      case "Progress":
+        downloaded += event.data.chunkLength;
+        console.log(`Downloaded ${downloaded} from ${contentLength}`);
+        break;
+      case "Finished":
+        console.log("Downloaded file size:", downloaded);
+        break;
+    }
+  });
+
+  console.log("Download completed successfully");
+
+  // 安装更新
+  console.log("Starting installation...");
+  await update.install();
+  console.log("Installation completed successfully");
+
+  // 重启应用程序
+  console.log("Attempting to relaunch...");
+  await relaunch();
+  console.log("Relaunch completed successfully");
+});
 </script>
 
-<template>
-
-</template>
+<template></template>
